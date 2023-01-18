ed48ffe096a76e6f79b78491b41c3cfd60f89870
Warner <lopezwarner0@gmail.com>
2023-01-17 22:10:45 -0600

    trabajando en el proecto de react

---
+++ src/Components/Formulario.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+class Formulario extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>
+
+                {/* <form>
+                    <div className="form-group">
+                        <label>nombre</label>
+                        <input className="form-control" type="text" name="name"></input>
+
+                    </div>
+                    
+                    <div className="form-group">
+                        <label>Edad</label>
+                        <input className="form-control" type="text" name="edad"></input>
+
+                    </div>
+                    <div className="form-group">
+                    <input className="btn btn-danger btn-block" type="submit">enviar</input>
+                       
+
+                    </div>
+                    
+
+                </form> */}
+
+            </div>
+        );
+    }
+}
+ 
+export default Formulario;
